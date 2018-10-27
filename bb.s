b863e60c1a5b9f0157144d63ac62a912231402f5
Edson Zandamela <edsonaguiar17@gmail.com>
2018-10-26 23:21:15 -0400

    Update bb.s

--- bb.s
+++ bb.s
@@ -1,15 +1,10 @@
-	.file	"bb.c"
-	.def	___main;	.scl	2;	.type	32;	.endef
-	.section .rdata,"dr"
 LC0:
 	.ascii "Hello World!\0"
 LC1:
 	.ascii "Welome to Assembly.\0"
 LC2:
 	.ascii "Testing Sentence.\0"
-	.text
 	.globl	_main
-	.def	_main;	.scl	2;	.type	32;	.endef
 _main:
 	pushl	%ebp
 	movl	%esp, %ebp
@@ -24,5 +19,3 @@ _main:
 	call	_puts
 	leave
 	ret
-	.ident	"GCC: (tdm64-1) 5.1.0"
-	.def	_puts;	.scl	2;	.type	32;	.endef
